b794fa2562deec0106d77640eb431ae45c92b9a4
villalbafrankin94-spec <villalbafrankin94@gmail.com>
2025-08-04 08:02:38 -0500

    cambios1

---
+++ style.py
@@ -0,0 +1,18 @@
+def calculadora():
+    opcion=1
+    num1 = opcion 
+    num2 = opcion
+    resltado= num1 + num2
+    opcion=2
+    num1 = opcion 
+    num2 = opcion
+    resltado= num1 - num2
+    opcion=3
+    num1 = opcion 
+    num2 = opcion
+    resltado= num1 * num2
+    opcion=4
+    num1 = opcion 
+    num2 = opcion
+    resltado= num1 / num2
+
